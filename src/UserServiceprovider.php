286bcda0f5181184c351a0f5e5f053198300ffe8
rajkale <raj.kale@neosofttech.com>
2018-08-08 14:04:51 +0530

    Second commit

--- src/UserServiceprovider.php
+++ src/UserServiceprovider.php
@@ -11,6 +11,13 @@ class UserServiceprovider extends ServiceProvider{
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/views', 'user');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->publishes([
+            __DIR__.'/database/migrations/' => database_path('migrations')
+        ], 'migrations');
+
+        $this->publishes([
+            __DIR__.'/views' => resource_path('views'),
+        ]);
     }
 
     public function register()
